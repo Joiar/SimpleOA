05b4442ac69c50b9797e056fb7874e7e425367eb
Carl <morrios@163.com>
2018-04-08 02:08:05 +0800

    [增加] 增加了登录功能及相关配置文件

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,15 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    // APP config
+    'APP_NAME' => 'OA',
+
+    // DB config
+    'DB_TYPE'               =>  'mysql',     // 数据库类型
+    'DB_HOST'               =>  '127.0.0.1', // 服务器地址
+    'DB_NAME'               =>  'SimpleOA',          // 数据库名
+    'DB_USER'               =>  'root',      // 用户名
+    'DB_PWD'                =>  'root',          // 密码
+    'DB_PORT'               =>  '3306',        // 端口
+    'DB_PREFIX'             =>  'OA_',    // 数据库表前缀
 );
---
+++ Application/Home/Model/UserModel.class.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Home\Model;
+
+use Think\Model;
+
+class UserModel extends Model
+{
+    public function doLogin($params)
+    {
+        $map = array(
+            'user_no' => $params['user_no']
+        );
+        $userInfo = $this->where($map)->field('user_id, password')->find();
+
+        if (!$userInfo || !password_verify($params['password'], $userInfo['password'])) {
+            return false;
+        }
+
+        self::storeUserInfo($userInfo['user_id']);
+        return true;
+    }
+
+    public function storeUserInfo($user_id)
+    {
+        $map = array(
+            'user_id' => $user_id
+        );
+        $userData = $this->where($map)
+            ->join('__DEPARTMENT__ ON __USER__.department_id = __DEPARTMENT__.department_id')
+            ->join('__POSITION__ ON __USER__.position_id = __POSITION__.position_id')
+            ->field('user_id,user_name,user_no,department_name,position_name,OA_USER.created_at')
+            ->find();
+        session('UserData', $userData);
+    }
+}
